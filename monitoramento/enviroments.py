f483a2f2c82d85ed3a8de44828de422faa4a14c2
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-16 21:53:21 -0300

    commit 21:46 16-02

--- monitoramento/enviroments.py
+++ monitoramento/enviroments.py
@@ -1,7 +1,7 @@
 # API_IA = 'http://127.0.0.1:8888/'
-# API_IOT = 'http://backend-iot-env.eba-prhv4rz9.sa-east-1.elasticbeanstalk.com/'
+API_IOT = 'http://backend-iot-env.eba-prhv4rz9.sa-east-1.elasticbeanstalk.com/'
 
-API_IOT = 'http://127.0.0.1:8888/'
+#API_IOT = 'http://127.0.0.1:8888/'
 API_IA = 'http://127.0.0.1:8801/'
 
 #Database aws
